12b15bb7ee5042a6d8567bec7463c513add8b393
ToasterVan <henrygras@icloud.com>
2023-11-13 17:56:13 +0100

    Refactor tests to match new code structure

--- Obliger - originale/Oblig5/oppg2.tests.py
+++ Obliger - originale/Oblig5/oppg2.tests.py
@@ -1,16 +1,17 @@
 import oppg2
 
-def test_lagBrukernavn():
-    result_username = oppg2.lagBrukernavn("Kari Nordmann", {})
+def test_append_user():
+    test_obj = oppg2.UserList()
+    test_obj.append_user("Kari Nordmann", "student.matnat.uio.no")
+
     expected_username = "karin"
+    expected_email = "karin@student.matnat.uio.no"
 
+    result_username = list(test_obj.users.keys())[0]
+    result_email = test_obj.users[expected_username]["email"]
+    
     assert result_username == expected_username, "Expected " + expected_username + ", but got " + result_username
-
-def test_lagEpost():
-    result_username = oppg2.lagEpost(oppg2.lagBrukernavn("Kari Nordmann", {}), "student.matnat.uio.no")
-    expected_username = "karin@student.matnat.uio.no"
-
-    assert result_username == expected_username, "Expected " + expected_username + ", but got " + result_username
+    assert result_email == expected_email, "Expected " + expected_email + ", but got " + result_email
 
 def test_user_list():
     result_UserList = oppg2.UserList()
@@ -19,8 +20,7 @@ def test_user_list():
 
     assert result_users_unwrapped == expected_users, f'Expected {expected_users}, but got {result_users_unwrapped}'
 
-test_lagBrukernavn()
-test_lagEpost()
 test_user_list()
+test_append_user()
 
 print("success")
