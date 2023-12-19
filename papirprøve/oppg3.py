f2f740b98ccb1cf7738135638cdabbded25a10ab
ToasterVan - School <108506830+ToasterVan@users.noreply.github.com>
2023-12-19 08:32:01 +0100

    Update oppg3.py

--- papirprøve/oppg3.py
+++ papirprøve/oppg3.py
@@ -15,3 +15,27 @@ def tell_forekomst(ord1: str, ord2: str, bokstav: chr):
     return "likt"
 
 print(tell_forekomst("banan", "alle", "a"))
+
+def tell_forekomst(ord: str, bokstav: chr):
+    count = 0
+
+    for l in ord:
+        if l == bokstav:
+            count += 1
+    
+    return count
+
+class Russebuss:
+    def __init__(self, plasser: int, antall_personer: int):
+        self.plasser = plasser
+        self.antall_personer = antall_personer
+
+    def get_personer(self):
+        return self.antall_personer
+
+    def slipp_på(self, antall_personer):
+        if self.plasser <= self.antall_personer + antall_personer:
+            self.antall_personer = self.plasser
+            return "Bussen er full"
+
+        self.antall_personer += antall_personer
