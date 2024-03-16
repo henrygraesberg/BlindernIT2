6bb31f7b652b8d3e741922d0bfb3f0a45956faab
ToasterVan <henrygras@icloud.com>
2024-03-16 15:40:04 +0100

    Add yellow printing

--- pygame/connect4/color_print.py
+++ pygame/connect4/color_print.py
@@ -1,5 +1,8 @@
-def print_red(skk):
-    print("\033[91m {}\033[00m".format(skk))
+def print_red(string):
+    print("\033[91m {}\033[00m".format(string))
 
-def print_green(skk):
-    print("\033[92m {}\033[00m".format(skk))
+def print_green(string):
+    print("\033[92m {}\033[00m".format(string))
+
+def print_yellow(string):
+    print("\033[93m {}\033[00m".format(string))
