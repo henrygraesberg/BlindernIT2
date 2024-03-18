4e4ad347835148b374ae9c4d0c06cb8a1847908b
ToasterVan - School <108506830+ToasterVan@users.noreply.github.com>
2024-03-18 10:29:00 +0100

    Remove leading space in color_print functions

--- pygame/connect4/color_print.py
+++ pygame/connect4/color_print.py
@@ -1,8 +1,8 @@
 def print_red(string):
-    print("\033[91m {}\033[00m".format(string))
+    print("\033[91m{}\033[00m".format(string))
 
 def print_green(string):
-    print("\033[92m {}\033[00m".format(string))
+    print("\033[92m{}\033[00m".format(string))
 
 def print_yellow(string):
-    print("\033[93m {}\033[00m".format(string))
+    print("\033[93m{}\033[00m".format(string))
