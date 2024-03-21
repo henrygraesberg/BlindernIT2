83f802a854dee7f7c2396c600237d4de4c63d997
ToasterVan - School <108506830+ToasterVan@users.noreply.github.com>
2024-03-21 10:21:31 +0100

    Add Blue

--- pygame/connect4/color_print.py
+++ pygame/connect4/color_print.py
@@ -1,8 +1,11 @@
-def print_red(string):
-    print(f'\033[91m{string}\033[00m')
+def print_red(string, sep=" ", end="\n"):
+    print(f'\033[91m{string}\033[00m', sep=sep, end=end)
 
-def print_green(string):
-    print(f'\033[92m{string}\033[00m')
+def print_green(string, sep=" ", end="\n"):
+    print(f'\033[92m{string}\033[00m', sep=sep, end=end)
 
-def print_yellow(string):
-    print(f'\033[93m{string}\033[00m')
+def print_yellow(string, sep=" ", end="\n"):
+    print(f'\033[93m{string}\033[00m', sep=sep, end=end)
+
+def print_blue(string, sep=" ", end="\n"):
+    print(f'\033[94m{string}\033[00m', sep=sep, end=end)
